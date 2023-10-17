eb35400a9bb7a9d8acfaeab549f24470cec28e52
SitiAisahW <141123782+SitiAisahW@users.noreply.github.com>
2023-10-17 14:28:57 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new typed(".text", {
+    stringS: ["Frontend Developer", "Youtuber" , "Web Developer"],
+    type5peed: 100,
+    back5peed: 100,
+    back5Delay: 1000,
+    loop:true
+});
